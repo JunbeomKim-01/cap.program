8530e21aedff04699baed859e551db7ed89e6dee
unknown <estjunbeom@gmail.com>
2020-10-22 14:36:04 +0900

    2020_10_22

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picbScreen = new System.Windows.Forms.PictureBox();
+            this.stsMenu = new System.Windows.Forms.StatusStrip();
+            this.tsslText = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.picbScreen)).BeginInit();
+            this.stsMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picbScreen
+            // 
+            this.picbScreen.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.picbScreen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picbScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picbScreen.Location = new System.Drawing.Point(0, 0);
+            this.picbScreen.Name = "picbScreen";
+            this.picbScreen.Size = new System.Drawing.Size(800, 450);
+            this.picbScreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picbScreen.TabIndex = 0;
+            this.picbScreen.TabStop = false;
+            this.picbScreen.Click += new System.EventHandler(this.picbScreen_Click);
+            // 
+            // stsMenu
+            // 
+            this.stsMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.stsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslText});
+            this.stsMenu.Location = new System.Drawing.Point(0, 424);
+            this.stsMenu.Name = "stsMenu";
+            this.stsMenu.Size = new System.Drawing.Size(800, 26);
+            this.stsMenu.TabIndex = 1;
+            this.stsMenu.Text = "statusStrip1";
+            // 
+            // tsslText
+            // 
+            this.tsslText.Name = "tsslText";
+            this.tsslText.Size = new System.Drawing.Size(270, 20);
+            this.tsslText.Text = "화면캡쳐: c , 화면지우기: e, 캡쳐저장: s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stsMenu);
+            this.Controls.Add(this.picbScreen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "화면캡쳐";
+            this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.picbScreen)).EndInit();
+            this.stsMenu.ResumeLayout(false);
+            this.stsMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picbScreen;
+        private System.Windows.Forms.StatusStrip stsMenu;
+        private System.Windows.Forms.ToolStripStatusLabel tsslText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Point orgLocalPoint;
+        Size orgLocalSize;
+        bool orgbool = true; 
+        bool capbool = false;  
+        Graphics ScreenG; 
+        Bitmap Captwin; 
+        public Form1()
+        {InitializeComponent();}
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {  }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e){
+            if (e.KeyChar == 'c'){
+                orgbool = false;//폼의 위치가 바뀌는 것을 감지하여 ture로 설정
+                capbool = true; //초기설정을 false로 바꾸고 캡쳐가 될경우 true가 되게 함
+                this.Opacity = 0.0; //폼의 투명도를 설정하는 구문
+                this.FormBorderStyle = FormBorderStyle.None; //Borderstyle 속성을 설정하는 것으로 폼 테두리가 보이지 않게 None
+                this.Location = new Point(0, 0);
+                this.Size = Screen.PrimaryScreen.Bounds.Size;
+                var fullScreen = Screen.PrimaryScreen.Bounds;
+                Captwin = new Bitmap(fullScreen.Width, fullScreen.Height);
+                ScreenG = Graphics.FromImage(Captwin);
+                ScreenG.CopyFromScreen(PointToScreen(new Point(0, 0)),
+                        new Point(0, 0), fullScreen.Size);
+                this.picbScreen.Image = Captwin;
+                this.Opacity = 100.0;
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                this.Location = orgLocalPoint;
+                this.Size = orgLocalSize;
+                orgbool = true;
+            }
+            else if (e.KeyChar == 'e'){
+                capbool = false;
+                this.picbScreen.Image = null;
+            }
+            else if (e.KeyChar == 's') {
+                if (capbool == true){
+                    using (var SFile = new SaveFileDialog()){
+                        SFile.OverwritePrompt = true;
+                        SFile.FileName = "화면캡쳐";
+                        SFile.Filter = "이미지 파일(*.jpg)|*.jpg";
+                        DialogResult rst = SFile.ShowDialog();
+                        if (rst == DialogResult.OK)
+                            Captwin.Save(SFile.FileName, ImageFormat.Jpeg);
+                    }
+                }
+                else{
+                    MessageBox.Show("화면을 캡쳐할 수 없음", "알림",
+                 MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void Form1_LocationChanged(object sender, EventArgs e){
+            if (orgbool == true){
+                orgLocalPoint = this.Location;
+                orgLocalSize = this.Size;
+            }
+        }
+        private void picbScreen_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
