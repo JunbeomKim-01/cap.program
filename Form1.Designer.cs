63143f51ad47156593d375a30f672fb50b77c29c
unknown <estjunbeom@gmail.com>
2020-10-29 08:32:23 +0900

    2020_10_29

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picbScreen = new System.Windows.Forms.PictureBox();
             this.stsMenu = new System.Windows.Forms.StatusStrip();
             this.tsslText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.picbScreen)).BeginInit();
             this.stsMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // picbScreen
@@ -58,18 +63,43 @@
             this.stsMenu.Size = new System.Drawing.Size(800, 26);
             this.stsMenu.TabIndex = 1;
             this.stsMenu.Text = "statusStrip1";
+            this.stsMenu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.stsMenu_ItemClicked);
             // 
             // tsslText
             // 
             this.tsslText.Name = "tsslText";
-            this.tsslText.Size = new System.Drawing.Size(270, 20);
-            this.tsslText.Text = "화면캡쳐: c , 화면지우기: e, 캡쳐저장: s";
+            this.tsslText.Size = new System.Drawing.Size(355, 20);
+            this.tsslText.Text = "화면확대: d, 화면캡쳐: c , 화면지우기: e, 캡쳐저장: s";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(728, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(790, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 398);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.stsMenu);
             this.Controls.Add(this.picbScreen);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -83,16 +113,20 @@
             ((System.ComponentModel.ISupportInitialize)(this.picbScreen)).EndInit();
             this.stsMenu.ResumeLayout(false);
             this.stsMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        
         private System.Windows.Forms.PictureBox picbScreen;
         private System.Windows.Forms.StatusStrip stsMenu;
         private System.Windows.Forms.ToolStripStatusLabel tsslText;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,33 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Drawing.Imaging;
+using System.Timers;
+using System.Threading;
+using System.Reflection.Emit;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        double rate = 2.0;
         Point orgLocalPoint;
         Size orgLocalSize;
         bool orgbool = true; 
         bool capbool = false;  
         Graphics ScreenG; 
         Bitmap Captwin; 
-        public Form1()
-        {InitializeComponent();}
+        public Form1(
+            )
+        {InitializeComponent();
+
+            timer1.Enabled = true;
+
+            this.MouseWheel += new MouseEventHandler(Form_MouseWheel);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+{
+            label1.Text = "배율 : x" + rate;
+
+            Size size = new Size(pictureBox1.Width, pictureBox1.Height);
+
+            Bitmap bitmap = new Bitmap((int)(size.Width / rate), (int)(size.Height / rate));
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            graphics.CopyFromScreen((int)(MousePosition.X - size.Width / (rate * 2)), (int)(MousePosition.Y - size.Height / (rate * 2)), 0, 0, size, CopyPixelOperation.SourceCopy);
+
+            pictureBox1.Image = bitmap;
+        }
+
+        private void Form_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta < 0 && rate > 0.2)
+            {
+                rate -= 0.1;
+            }
+            else if (e.Delta > 0 && rate < 5.0)
+            {
+                rate += 0.1;
+            }
+        }
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {  }
-        private void Form1_KeyPress(object sender, KeyPressEventArgs e){
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            int curX = Cursor.Position.X;
+            int curY = Cursor.Position.Y;
+
+            int screenWidth = 50;
+            int screeHeight = 50;
+
+            Size size = new Size(screenWidth, screeHeight);
+            Bitmap bitmap = new Bitmap(size.Width, size.Height);
+
+            double rate = 2.0;
+
+            Graphics graphic = Graphics.FromImage(bitmap);
+            graphic.CopyFromScreen(curX - (screenWidth / 2), curY - (screeHeight / 2), 0, 0, size);
+
+            int zoominWidth = (int)(screenWidth * rate);
+            int zoominHeight = (int)(screeHeight * rate);
+
+            Bitmap zoomin = new Bitmap(zoominWidth, zoominHeight);
+
+            for (int i = 0; i < zoominWidth; i++)
+            {
+                for (int j = 0; j < zoominHeight; j++)
+                {
+                    int row = (int)(i / rate);
+                    int col = (int)(j / rate);
+                    zoomin.SetPixel(i, j, bitmap.GetPixel(row, col));
+                }
+            }
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e) {
+            if(e.KeyChar =='d')
+            {
+                label1.Text = "배율 : x" + rate;
+
+                Size size = new Size(picbScreen.Width, picbScreen.Height);
+
+                Bitmap bitmap = new Bitmap((int)(size.Width / rate), (int)(size.Height / rate));
+                Graphics graphics = Graphics.FromImage(bitmap);
+
+                graphics.CopyFromScreen((int)(MousePosition.X - size.Width / (rate * 2)), (int)(MousePosition.Y - size.Height / (rate * 2)), 0, 0, size, CopyPixelOperation.SourceCopy);
+
+                picbScreen.Image = bitmap;
+            }
             if (e.KeyChar == 'c'){
                 orgbool = false;//폼의 위치가 바뀌는 것을 감지하여 ture로 설정
                 capbool = true; //초기설정을 false로 바꾸고 캡쳐가 될경우 true가 되게 함
                 this.Opacity = 0.0; //폼의 투명도를 설정하는 구문
+
                 this.FormBorderStyle = FormBorderStyle.None; //Borderstyle 속성을 설정하는 것으로 폼 테두리가 보이지 않게 None
                 this.Location = new Point(0, 0);
                 this.Size = Screen.PrimaryScreen.Bounds.Size;
@@ -65,6 +143,8 @@ namespace WindowsFormsApp1
             }
         }
 
+
+
         private void Form1_LocationChanged(object sender, EventArgs e){
             if (orgbool == true){
                 orgLocalPoint = this.Location;
@@ -72,6 +152,25 @@ namespace WindowsFormsApp1
             }
         }
         private void picbScreen_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stsMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
